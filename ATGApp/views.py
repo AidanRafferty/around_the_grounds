782e0b64d2fb364407d7261ec26d99b604dc88df
Jack McKechnie <2319658m@student.gla.ac.uk>
2019-02-28 17:43:17 +0000

    Worked on Index view

--- ATGApp/views.py
+++ ATGApp/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from ATGApp.forms import Review
+from ATGApp.models import Review
 # Create your views here.
 
 
 def index(request):
-    #Needs highest rated stadium
-    #Order TotalScore and choose top
-    #Return in context dictionary
-
-    return HttpResponse("The ATG App Index view")
+    #Returns information on highest rated stadium
+    #Returns picture of highest rated stadium
+    
+    highestRatedStadium = Review.objects.order_by('-totalScore')[:1]
+    context_dict = {'highestRatedStadium' : highestRatedStadium}
+    response = render(request,'atg/index.html',context = context_dict)
+    return response
 
 
 
