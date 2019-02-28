3d3c0ccdda3b715e9eeb3aec74e9675eb0e0bdf9
Jack McKechnie <2319658m@student.gla.ac.uk>
2019-02-28 13:18:11 +0000

    Start on index view

--- ATGApp/views.py
+++ ATGApp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from atg.forms import Review
 # Create your views here.
 
 
 def index(request):
+    #Needs highest rated stadium
+    #Order TotalScore and choose top
+    #Return in context dictionary
 
     return HttpResponse("The ATG App Index view")
+
+
 
 
