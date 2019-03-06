4aadeedb8ec1958d7af79a071d2d22659acc6cdb
Aidan Rafferty <2314722r@student.gla.ac.uk>
2019-03-06 19:13:09 +0000

    Fixed an error in re-calculating total score and average score for stadiums whenone of their reviews are deleted

--- ATGApp/models.py
+++ ATGApp/models.py
@@ -31,10 +31,10 @@ class UserProfile(models.Model):
 
 
 class Stadium(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, unique=True, primary_key=True)
-    photo = models.ImageField(upload_to='stadium_images', blank = True)
-    capacity = models.IntegerField(default = 0)
+    photo = models.ImageField(upload_to='stadium_images', blank=True)
+    capacity = models.IntegerField(default=0)
     postcode = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
     homeTeam = models.CharField(max_length=55)
@@ -42,39 +42,42 @@ class Stadium(models.Model):
     ReviewCount = models.IntegerField(default=0)
     TotalScore = models.IntegerField(default=0)
     averageScore = models.IntegerField(default=0)
-    
 
-    def save(self,*args, **kwargs):
-        
+    def save(self, *args, **kwargs):
+
         # create a counter storing the number of review objects associated with this stadium being saved
         # assign the number of reviews for this stadium to counter and store this as the
-        # review count field value for this stadium. 
+        # review count field value for this stadium.
         counter = 0
         counter = Review.objects.all().filter(stadium=self).count()
         self.ReviewCount = counter
 
-        # keep a count of the total score 
+        # keep a count of the total score
         score = 0
         # if there are reviews on the stadium
-        if counter>0:
+        if counter > 0:
             # get the reviews on this stadium
             reviews = Review.objects.all().filter(stadium=self)
             # fir each review
             for r in reviews:
-                # add the total score for the review to the counter 
+                # add the total score for the review to the counter
                 score += r.totalScore
             # when the loop has completed assign the scores across all reviews to the total
-            # score of the stadium 
-            self.TotalScore = score
-        
-        self.averageScore = self.TotalScore//self.ReviewCount
+            # score of the stadium
+        self.TotalScore = score
+
+        if self.ReviewCount !=0:
+
+            self.averageScore = self.TotalScore//self.ReviewCount
+       
+        else:
+            
+            self.averageScore = 0
 
 
-        self.slug = slugify(self.name)         
+        self.slug = slugify(self.name)
 
         super(Stadium, self).save(*args, **kwargs)
-     
-        
 
     class Meta:
         verbose_name_plural = 'stadiums'
@@ -83,61 +86,39 @@ class Stadium(models.Model):
         return self.name
 
     def __repr__(self):
-        return self.name 
+        return self.name
+
 
 class Review(models.Model):
-    id = models.AutoField(primary_key=True) 
-    user = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
-    stadium = models.ForeignKey(Stadium, on_delete = models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE)
     atmosphere = models.IntegerField(default=0)
     food = models.IntegerField(default=0)
     facilities = models.IntegerField(default=0)
-    additionalInfo = models.CharField(max_length = 200)
+    additionalInfo = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now=True)
-    totalScore = models.IntegerField(blank = True)
-    
-    
+    totalScore = models.IntegerField(blank=True)
 
     def save(self, *args, **kwargs):
-        
-        self.date=date.today()
+
+        self.date = date.today()
 
         self.totalScore = self.atmosphere + self.food + self.facilities
         super(Review, self).save(*args, **kwargs)
         Stadium.save(self.stadium)
-        
+
     def __str__(self):
         return str(self.id)
 
     def __repr__(self):
-        return str(self.id) 
-    
+        return str(self.id)
+
     @receiver(post_delete)
     def save_stadium_again(sender, instance, **kwargs):
         if sender == Review:
             instance.stadium.save()
 
 
-    
-
-
-
-
 # in order to update the data in the tables when a user deletes their account or
 # reviews or stadi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
