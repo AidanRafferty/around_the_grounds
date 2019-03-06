6866dfac96b8c56c58abba4a8ce802c36e38c0e3
Gabriel Georgiev <2317806g@student.gla.ac.uk>
2019-03-06 18:04:11 +0000

    register page now has a form

--- ATGApp/views.py
+++ ATGApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ATGApp.models import Review, Stadium
+from ATGApp.forms import UserForm, UserProfileForm
 
 def index(request):
     #Returns information on highest rated stadium
@@ -22,8 +23,36 @@ def login(request):
     return render(request, 'ATGApp/login.html', context = context_dict)
 
 def register(request):
-    context_dict = {}
-    return render(request, 'ATGApp/register.html', context = context_dict)
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'atgapp/register.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form,
+                   'registered': registered})
 
 def account(request):
     context_dict = {}
