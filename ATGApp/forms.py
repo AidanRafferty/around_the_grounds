ce724a32b570248dbdc006d7c1fdd8710dd4c75a
Robbie-Thomson <2314819t@student.gla.ac.uk>
2019-03-07 20:26:37 +0000

    review form

--- ATGApp/forms.py
+++ ATGApp/forms.py
@@ -15,6 +15,28 @@ class StadiumForm(forms.ModelForm):
         model = Stadium
         fields = ('name','capacity', 'postcode', 'image')
 
+Number_Choices=[
+    (1,'Unacceptable),
+    (2,'Poor),
+    (3,'Satisfactory),
+    (4,'Good),
+    (5,'Exellent),
+]
+
+class ReviewForm(forms.ModelForm):
+    stadiumName = forms.CharField(Stadium._meta.get_field("stadiumName").max_length, help_text="Please enter the Stadium name.")
+    atmosphere = forms.IntegerField(label="Atmosphere:", widgets=forms.RadioSelect(choices=Number_Choices))
+    food = forms.IntegerField(label="Food:", widgets=forms.RadioSelect(choices=Number_Choices))
+    facilities = forms.IntegerField(label="Facilities:", widgets=forms.RadioSelect(choices=Number_Choices))
+#additional comments
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # An inline class to provide additional information on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Stadium
+        fields = ('stadiumName','atmosphere', 'food', 'facilities', 'additionalComments')
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
