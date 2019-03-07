1519253392c2f34442f8b30b5f5c4c9ed940737d
Robbie-Thomson <2314819t@student.gla.ac.uk>
2019-03-07 20:08:39 +0000

    stadium form

--- ATGApp/forms.py
+++ ATGApp/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from ATGApp.models import Review, Stadium, UserProfile
 from django.contrib.auth.models import User
 
+class StadiumForm(forms.ModelForm):
+    name = forms.CharField(Stadium._meta.get_field("name").max_length, help_text="Please enter the Stadium name.")
+    capacity = forms.IntegerField(Stadium._meta.get_field("capacity").max_length, help_text="Please the Stadiums Capacity.")
+    postcode = forms.CharField(Stadium._meta.get_field("name").max_length, help_text="Please enter the Postcode of the Stadium.")
+    #image input
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # An inline class to provide additional information on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Stadium
+        fields = ('name','capacity', 'postcode', 'image')
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
