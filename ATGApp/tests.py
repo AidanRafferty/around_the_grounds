85ef9b43c2cf6ebe8178e662c59a1a51a3154415
Jack McKechnie <2319658m@student.gla.ac.uk>
2019-03-18 10:54:10 +0000

    Start on tests

--- ATGApp/tests.py
+++ ATGApp/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from ATGApp.models import Stadium,UserProfile,User
+from django.core.urlresolvers import reverse
+import populate_ATGApp
 
-# Create your tests here.
+def create_user():
+    # Create a user
+    user = User.objects.get_or_create(username="testuser", password="test1234",
+                                      first_name="Test", last_name="User", email="testuser@testuser.com")[0]
+    user.set_password(user.password)
+    user.save()
+
+    # Create a user profile
+    user_profile = UserProfile.objects.get_or_create(user=user)[0]
+    user_profile.save()
+
+    return user, user_profile
+
+class StadiumTests(TestCase):
+
+    def test_stadium_name_slug(self):
+        stadium = Stadium()
+        stadium.name = 'Best Stadium In The World'
+        stadium.user_id = 1
+        stadium.save()
+        self.assertEqual(stadium.slug,"best-stadium-in-the-world")
+
+    def test_stadium_view(self):
+        populate_ATGApp.populate()
+        response = self.client.get(reverse('stadiums'))
+        self.assertContains(response, "Best Stadium In The World")
+        
+        
+
+        
+        
+        
